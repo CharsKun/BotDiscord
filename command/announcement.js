850a33ae170e432edd4d6ac66f2374e3c0e7f348
CharsKun <charskun09@gmail.com>
2021-02-21 15:25:39 +0700

    announcement 3

--- command/announcement.js
+++ command/announcement.js
@@ -4,45 +4,29 @@ module.exports = {
 	execute(message,args,Discord){
 		const newEmbed = new Discord.MessageEmbed()
 		.setColor('#0099ff')
-		.setTitle('OFFROAD MEET EVENT')
+		.setTitle('TEAM DEATH MATCH EVENT')
 		//.setDescription(`--Halo kalian @everyone--\nUntuk event 3 minggu kedepan ada yang mau kasi saran?\nkalo ada boleh kasi tau ke admin\ntar kalo cocok akan dipilah dan difilter lagi\n\n----------------\nSetelah Event 3 minggu kedepan\nakan ada event menarik yaitu Event Race Car\nnamun dengan mobil yang sama\nReward:\n100k Pulsa pure dari kantong sendiri\n\nDengan syarat:\nYang make mod akan didiskualifikasi & tentunya akan di ban dari server.`)
 		.setDescription(`
 			Halo @everyone\n\n
-			\nHari, Tanggal	: Jumat, 19 Februari 2020
-			\nWaktu			: 21.00 WIB - Selesai
-			\nTitik Kumpul 	: Los Santos Costume
-			\nOutfit		: Bebas, asal ga aneh-aneh
-			\n\nNo Respawn at all, Challange your skill, Arena war vehicle are not allowed.\n
-			Jika kendaraan kalian hancur/rusak, pilihanmu hanyalah berhenti atau numpang dengan kendaraan teman.\n\n
-			VEHICLE LIST\n\n
-			SOUTHERN SAN ANDREAS :\n
-			- CANIS SEMINOLE FRONTIER\n
-			- DUNDREARY LANDSTALKER\n
-			- KARIN EVERON\n
-			- ANNIS HELLION\n
-			- VAPID CARACARA 4X4\n
-			- KARIN BEEJAY XL\n
-			- FATHOM FQ 2\n
-			- BENEFACTOR SERRANO\n
-			- MAMMOTH PATRIOT\n
-			- CANIS KAMACHO\n
-			- VAPID RIATA\n
-			- VAPID CONTENDER\n
-			- VAPID GUARDIAN\n
-			- BF INJECTION\n
-			- VAPID SANDKING\n
-			- VAPID SANDKING SWB\n
-			- BRAVADO BISON\n
-			- CANIS BODHI\n
-			- KARIN REBEL (CLEAN)\n
-			- DECLASSE RANCHER XL\n
-			- DECLASSE GRANGER\n
-			- GALLIVANTER BALLER\n
-			- ALBANY CAVALCADE\n\n
-			RULES :\n
-			Jangan rusuh, Jangan PK (Player Killer), No Baper\n
-			Cukup ikuti arahan kami :D\n\n
+			
+			Untuk Jumat ini kami akan mengadakan event Team DeathMatch 2v2\n
+			Daftarkan tim kalian ke @SON OF KRONOS\n\n
+			\nHari, Tanggal	: Jumat, 26 Februari 2020
+			\nWaktu			: 20.00 WIB - Selesai
+			\nTitik Kumpul 	: Shandy Shores Airfield
+			\n\n
+			Note:\n
+			Tidak semua senjata yang diperbolehkan, senjata yang diperbolehkan hanya Assault Rifle, Sub MachineGun, Pistol, Sniper.\n
+			Selain senjata yang disana tidak diperbolehkan termasuk MK2 Weapon, Throwable, Explosion, Up & Automizer, MachineGun.\n
+			Setiap tim akan bertanding untuk menjadi juara, Challange your skills.\n
+			Arena pertempuran akan dipersiapkan oleh Kami :D\n\n
 
+			RULES :\n
+			Untuk para spectator atau penonton jangan ikut membantu atau rusuh.\n
+			Jangan menggunakan senjata yang tidak diperbolehkan, jika dilanggar maka pemain tersebut dianggap kalah.\n
+			\n
+			Aturan permainan dapat berubah seiring pertandingan.\n
+			Untuk pemberhantuan lebih lanjut akan kami sampaikan nanti.\n\n
 			Thank you.\n
 
 			`)
