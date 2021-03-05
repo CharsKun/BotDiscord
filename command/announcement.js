6608dd7e2597dc4c5325aa5f693444c5bc8ba333
CharsKun <charskun09@gmail.com>
2021-03-05 22:21:12 +0700

    announcement 4.2

--- command/announcement.js
+++ command/announcement.js
@@ -4,15 +4,14 @@ module.exports = {
 	execute(message,args,Discord){
 		const newEmbed = new Discord.MessageEmbed()
 		.setColor('#0099ff')
-		.setTitle('TEAM DEATH MATCH EVENT')
+		.setTitle('RACING EVENT - REANNOUNCE')
 		//.setDescription(`--Halo kalian @everyone--\nUntuk event 3 minggu kedepan ada yang mau kasi saran?\nkalo ada boleh kasi tau ke admin\ntar kalo cocok akan dipilah dan difilter lagi\n\n----------------\nSetelah Event 3 minggu kedepan\nakan ada event menarik yaitu Event Race Car\nnamun dengan mobil yang sama\nReward:\n100k Pulsa pure dari kantong sendiri\n\nDengan syarat:\nYang make mod akan didiskualifikasi & tentunya akan di ban dari server.`)
 		.setDescription(`
 			Halo @everyone\n\n
-			
-			Untuk Jumat ini kami akan mengadakan event Racing yang berhadiah 100K Pulsa.\n
+			Maaf buat pemain tadi yang sudah hadir, kami mengucapkan terima kasih dan mohon maaf sebesar-besarnya karena terjadi kendala, karena kesalahan jaringan / network, yang dimana beberapa pemain tidak dapat join, maka dari itu kami mengundurkan jadwal event ke hari esok.\n
 			\n
-			\nHari, Tanggal	: Jumat, 5 Maret 2020
-			\nWaktu			: 20.00 WIB - Selesai
+			\nHari, Tanggal	: Sabtu, 6 Maret 2020
+			\nWaktu			: 21.00 WIB - Selesai
 			\nTitik Kumpul 	: Los Santos Airport
 			\n\n
 			Rules:\n
@@ -21,7 +20,7 @@ module.exports = {
 			Pemain tidak boleh menggunakan kendaraan masing-masing. dan pemain tidak boleh menggunakan mod / cheat, jika ketahuan maka akan didiskualifikasi.\n\n
 
 			Note:\n
-			Sebelum pertandingan semua pemain prepare pada jam 18.00 / 19.00 membawa kendaraan supercar / sportnya dengan menggunakan modif seindah mungkin & menggunakan dresscode masing-masing. 
+			Sebelum pertandingan semua pemain prepare pada jam 19.00 - 21.00 membawa kendaraan supercar / sportnya dengan menggunakan modif seindah mungkin & menggunakan dresscode masing-masing. 
 			`)
 		.setImage('https://prod.cloud.rockstargames.com/crews/sc/2612/54629768/publish/emblem/emblem_512.png')
 		.setFooter('Salam E-Conspiracy.');
