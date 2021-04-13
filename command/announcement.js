a03bdee441b6e5db6ebb9d31ca292cced08e5463
CharsKun <63503097+CharsKun@users.noreply.github.com>
2021-04-13 17:02:40 +0700

    motogp

--- command/announcement.js
+++ command/announcement.js
@@ -5,21 +5,19 @@ module.exports = {
 		const newEmbed = new Discord.MessageEmbed()
 		.setColor('#0099ff')
 
-		.setTitle('SHORT MOVIE')
+		.setTitle('MotoGP')
 		//.setDescription(`--Halo kalian @everyone--\nUntuk event 3 minggu kedepan ada yang mau kasi saran?\nkalo ada boleh kasi tau ke admin\ntar kalo cocok akan dipilah dan difilter lagi\n\n----------------\nSetelah Event 3 minggu kedepan\nakan ada event menarik yaitu Event Race Car\nnamun dengan mobil yang sama\nReward:\n100k Pulsa pure dari kantong sendiri\n\nDengan syarat:\nYang make mod akan didiskualifikasi & tentunya akan di ban dari server.`)
 		.setDescription(`
 			Halo @everyone\n\n
 			
-			Event weekly kali ini kita akan membuat short movie lagi yang bertemakan perang di daerah hutan (Paleto Forest). Untuk story, tema, dll akan kami beritahukan lebih lanjut.\n\n
+			Event weekly kali ini kita akan mengadakan event Racing MotoGP, untuk detail event akan diberitahukan lebih lanjut.\n\n
 
-			\nHari, Tanggal	: Jumat, 12 Maret 2020
-			\nWaktu			: 20.00 WIB - Selesai
-			\nTitik Kumpul 	: Paleto Forest
+			\nHari, Tanggal	: Jumat, 16 April 2021
+			\nWaktu			: 21.00 WIB - Selesai
+			\nTitik Kumpul 	: Casino
 			\nOutfit		: Akan diberitahukan lebih lanjut
 
-			\n\n
-			Note:\n
-			Sebelum short movie dibuat diharapkan semua telah memakai pakaian atau outfit yang sudah diarahkan, jika belum maka di hari H- kita akan mengatur outfit bagi yang belum mempunyai outfit.
+			\n
 			`)
 		.setImage('https://prod.cloud.rockstargames.com/crews/sc/2612/54629768/publish/emblem/emblem_512.png')
 		.setFooter('Salam E-Conspiracy.');
